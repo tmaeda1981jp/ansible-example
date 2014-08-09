0dba7dd1c6b094908f05eb8239370823ef8828bd
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-08-10 00:08:21 +0900

    Add serverspec

---
+++ spec/ansible-test-server/php_spec.rb
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+
+require 'spec_helper'
+
+describe package('php') do
+  it { should be_installed.with_version '5.5' }
+end
+
+# describe package('php-mbstring') do
+#   it { should be_installed }
+# end
+
+# describe package('php-mysql') do
+#   it { should be_isntalled }
+# end
+
+# describe package('php-pdo') do
+#   it { should be_installed }
+# end
+
+# describe package('php-xml') do
+#   it { should be_installed }
+# end
