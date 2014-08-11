90ea80f30bf0b4dedcdaae24c2118e4431270eae
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-08-11 17:25:03 +0900

    Add nginx settigns

--- spec/ansible-test-server/nginx_spec.rb
+++ spec/ansible-test-server/nginx_spec.rb
@@ -13,7 +13,9 @@ describe port(80) do
   it { should be_listening }
 end
 
-# describe file('/etc/httpd/conf/httpd.conf') do
-#   it { should be_file }
-#   its(:content) { should match /ServerName ansible-test-server/ }
-# end
+describe file('/etc/nginx/conf.d/default.conf') do
+  it { should be_file }
+  its(:content) { should match(/root   \/var\/www;/) }
+  its(:content) { should match(/index  index.html index.htm index.php/) }
+  its(:content) { should match(/fastcgi_param  SCRIPT_FILENAME  \$document_root\$fastcgi_script_name;/) }
+end
