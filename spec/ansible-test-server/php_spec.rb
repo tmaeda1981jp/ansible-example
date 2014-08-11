515ab2f291a041cbfd6a808cc6b67df2237874a8
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-08-11 12:06:49 +0900

    Add playbook for installing php-fpm

--- spec/ansible-test-server/php_spec.rb
+++ spec/ansible-test-server/php_spec.rb
@@ -6,6 +6,10 @@ describe package('php') do
   it { should be_installed.with_version '5.5' }
 end
 
+describe package('php-fpm') do
+  it { should be_installed }
+end
+
 # describe package('php-mbstring') do
 #   it { should be_installed }
 # end
